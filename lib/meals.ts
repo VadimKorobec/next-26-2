e7efbc89df0cd69dd73c77b6345e5f8eac3f02ad
Vadim <_s_e_t__@ukr.net>
2026-03-17 16:36:45 +0200

    add some code

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,8 +1,13 @@
-import { Meal } from "@/types/meal";
 import sql from "better-sqlite3";
+
+import { Meal } from "@/types/meal";
 
 const db = sql("meals.db");
 
 export const getMeals = () => {
   return db.prepare("SELECT * FROM meals").all() as Meal[];
+};
+
+export const getMeal = async (slug: string) => {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as Meal;
 };
--- src/app/meals/[mealSlug]/page.tsx
+++ src/app/meals/[mealSlug]/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getMeal } from "../../../../lib/meals";
+
+import styles from "./page.module.css";
+
 interface MealPageProps {
   params: Promise<{ mealSlug: string }>;
 }
@@ -5,7 +12,29 @@ interface MealPageProps {
 const MealDetailsPage = async ({ params }: MealPageProps) => {
   const { mealSlug } = await params;
 
-  return <h1>Meal Details {mealSlug}</h1>;
+  const meal = await getMeal(mealSlug);
+
+  return !meal ? (
+    notFound()
+  ) : (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions}>{meal.instructions}</p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailsPage;
