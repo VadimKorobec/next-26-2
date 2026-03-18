18de7528950973943604d094b756e0fb31626332
VadimKorobec <_s_e_t__@ukr.net>
2026-03-18 15:42:01 +0200

    add some code in Share page

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -15,7 +15,11 @@ const ShareMealPage = () => {
         summary: formData.get("summary") as string,
         instructions: formData.get("instructions") as string,
         image: formData.get("image") as string,
+        creator: formData.get("name") as string,
+        creator_email: formData.get("email") as string,
       };
+
+      console.log(meal);
     }
   };
 
--- src/types/meal.ts
+++ src/types/meal.ts
@@ -1,10 +1,10 @@
 export type Meal = {
-    creator: string;
-    creator_email: string;
-    id: number;
-    image: string;
-    instructions: string;
-    slug: string;
-    summary: string;
-    title: string;
+  creator: string;
+  creator_email: string;
+  id?: number;
+  image: string;
+  instructions: string;
+  slug?: string;
+  summary: string;
+  title: string;
 };
